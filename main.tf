cb06f34ed88c01d4788a47618ede35a34ea8c97d
krishna-ca <krishna@cloudsanalytics.ai>
2025-07-14 14:03:58 +0530

    Auto-merged PR by n8n
    * Push Terraform files 🚀
    
    * Push Terraform files 🚀

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  region = "us-east-2"
+  region = "us-west-2"
 }
 
 resource "aws_instance" "example" {
   ami           = "ami-0c94855ba95c574c8"
-  instance_type = "t2.micro"
+  instance_type = "r6g.large"
 
   tags = {
     Name = "example-instance"
