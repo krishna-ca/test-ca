11e644cdab6241008b836ec6f721e988465d8c24
krishna-ca <krishna@cloudsanalytics.ai>
2025-07-14 15:01:25 +0530

    Auto-merged PR by n8n
    * Push Terraform files 🚀
    
    * Push Terraform files 🚀
    
    * Push Terraform files 🚀

--- variables.tf
+++ variables.tf
@@ -1,7 +1,5 @@
-variable "ami" {
-  default = "ami-0c94855ba95c574c8"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
+variable "key_name" {
+  description = "Name of the SSH key to be used for the EC2 instance."
+  type        = string
+  default     = "my-key-pair"
 }
